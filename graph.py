588a41c035463a996dedf0b7a9cec2e599a0222c
Sam Turner <98767222+stmio@users.noreply.github.com>
2024-01-11 17:45:56 +0000

    feat: update graph plotting script

--- graph.py
+++ graph.py
@@ -5,9 +5,16 @@ from numpy import linspace
 with open("./brownian.txt", "r") as f:
     data = f.readlines()
 data = [float(i.strip()) for i in data]
+data.append(0.0) 
 
 kde = gaussian_kde(data)
 dist_space = linspace(min(data), max(data), 100)
+
+plt.xlim([0, max(data)])
+plt.ylim([0, max(kde(dist_space)) + 0.0001])
+
+plt.xlabel("Speed (pixels per second)")
+plt.ylabel("Probability density")
 
 plt.plot(dist_space, kde(dist_space))
 plt.savefig("graph.png")
